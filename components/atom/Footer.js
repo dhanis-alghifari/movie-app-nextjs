47af524598785d0a779836cc326f35d59dbe7f81
dhanis-alghifari <dhanisalghifari@gmail.com>
2023-01-30 23:57:42 +0700

    update footer

--- components/atom/Footer.js
+++ components/atom/Footer.js
@@ -8,11 +8,10 @@ export default function Footer() {
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4 mx-auto text-center">
               <div className="text-sm text-slate-500 font-semibold py-1">
-                Copyright © {new Date().getFullYear()} Movie App by
-                <a className="text-slate-500 hover:text-slate-800">
-                  Dhanis Al Ghifari
-                </a>
-                .
+                Copyright © {new Date().getFullYear()} Movie App by&nbsp;
+                <span className="text-slate-500 hover:text-slate-800">
+                  Dhanis Al Ghifari.
+                </span>
               </div>
             </div>
           </div>
